147700f73aabe12b364b676e3b083f8ca35c2c08
vetordev <vitor13m2003@gmail.com>
2023-06-28 23:36:10 -0300

    new: match model

---
+++ domain/model/match/match.go
@@ -0,0 +1,25 @@
+package match
+
+import (
+	"cblol-bot/domain/model/team"
+	"time"
+)
+
+type State = int
+
+const (
+	Unstarted State = iota
+	Completed
+)
+
+type Match struct {
+	schedule time.Time
+	state    State
+
+	team1 team.Team
+	team2 team.Team
+}
+
+func New(schedule time.Time, state State, team1 team.Team, team2 team.Team) *Match {
+	return &Match{schedule, state, team1, team2}
+}
